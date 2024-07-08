52ffc51e79b5a7f459436e869056ba2674510d02
sswin1027 <sswin1027@gmail.com>
2024-07-08 20:14:02 +0900

    travel

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 let bars = document.querySelector("#bars")
 // console.log(bars)
+let nav = document.querySelector("nav")
 let navLinks = document.querySelector(".nav-links")
 // console.log(navLinks)
+
 bars.addEventListener("click", () => {
     // alert("hello")
     navLinks.classList.toggle("show")
     bars.classList.toggle("fa-times")
+})
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 30){
+        nav.classList.add("bgcolor")
+    }else{
+        nav.classList.remove("bgcolor")
+    }
 })
